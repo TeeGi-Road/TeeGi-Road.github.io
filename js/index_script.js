696d558d81e7f4c5a594f3fa610a686101cf6678
siaoyu <schobitsy@gmail.com>
2019-12-05 17:37:18 +0800

    modify index
    1. 調整index排版及按鈕
    2. 根據瀏覽寬度改變畫面
    3. 增加index按鈕圖片,背景影片

--- js/index_script.js
+++ js/index_script.js
@@ -1,21 +1,9 @@
-// 點擊下滑navbar
-$(document).ready(function () {
-    $('.nav2 li').click(function () {
-        if ($('.nav1')[0].style.display === 'block') {
-            $('.nav1').hide(3000);
-        } else {
-            $('.nav1').show(3000);
-        }
-    });
-});
-
-
 // 跑馬燈
-$(document).ready(function () {
-    $('.marquee').marquee({
-        pauseOnHover: true,
-    });
-});
+// $(document).ready(function () {
+//     $('.marquee').marquee({
+//         pauseOnHover: true,
+//     });
+// });
 
 
 // 滑鼠移入移出換圖
@@ -37,66 +25,109 @@ $(document).ready(function () {
 
 
 // 改變瀏覽器寬度時 add/remove class
-function windowSize() {
-    if ($(window).width() < 768) {
-        $('.index2').innerHTML += `
-        可馨要改的平板板HTML格式`;
-    } else if ($(window).width() < 1200) {
-        // navbar
-        $('.navbar .container button').attr('data-toggle', 'dropdown');
-        $('.navbar .container button').attr('aria-haspopup', 'true');
-        $('.navbar .container button').attr('data-target', '');
-        $('.navbar .container button').attr('aria-controls', '');
-        $('.navbar .container button').attr('aria-label', '');
+function mq() {
+    var query = Modernizr.mq('(max-width: 1199.98px)');
+    if (query) {
+        $('#holder').html('true');
+        // JavaScript here
+        // 當CSS media query計算的視窗寬度小於 px時執行
         $('.navbar .container div').addClass('dropdown-menu');
         $('.navbar .container div ul').addClass('dropdown-item');
-        // container
-        $('.index1 .row:nth-child(1) .col-6').addClass('col');
-        $('.index1 .row:nth-child(1) .col-6').removeClass('col-6');
-        $('.index1 .row:nth-child(2) .col-8').addClass('col');
-        $('.index1 .row:nth-child(2) .col-8').removeClass('col-8');
-        $('.index1 .row:nth-child(3) .col-2').removeClass('col-2');
-        $('.index1 .row:nth-child(3) .col-9').removeClass('col-9');
-        $('.index2').innerHTML += `
-        可馨要改的手機板HTML格式`;
+
+        // 手機版12欄位btn
+        document.querySelector('.index2').innerHTML = `
+        <div class="row">
+            <div class="col-6">
+                <a href="./Neiw.html"><img src="./img/index/index-btn-Neiw-mobi.jpg" alt="前進內灣線"></a>
+            </div>
+            <div class="col-6">
+                <a href="./Liuj.html"><img src="./img/index/index-btn-Liuj-mobi.jpg" alt="前進六家線"></a>
+            </div>
+            <div class="col-6">
+                <a href="./Jiji.html"><img src="./img/index/index-btn-Jiji-mobi.jpg" alt="前進集集線"></a>
+            </div>
+            <div class="col-6">
+                <a href="./Shal.html"><img src="./img/index/index-btn-Shal-mobi.jpg" alt="前進沙崙線"></a>
+            </div>
+            <div class="col-6">
+                <a href="./Shen.html"><img src="./img/index/index-btn-Shen-mobi.jpg" alt="前進深澳線"></a>
+            </div>
+            <div class="col-6">
+                <a href="./Ping.html"><img src="./img/index/index-btn-Ping-mobi.jpg" alt="前進平溪線"></a>
+            </div>
+        </div>`;
     } else {
-        // navbar
-        $('.navbar .container button').attr('data-toggle', 'collapse');
-        $('.navbar .container button').attr('aria-haspopup', '');
-        $('.navbar .container button').attr('data-target', '#navbarSupportedContent');
-        $('.navbar .container button').attr('aria-controls', 'navbarSupportedContent');
-        $('.navbar .container button').attr('aria-label', 'Toggle navigation');
+        $('#holder').html('false');
+        // JavaScript here
+        // 當CSS media query計算的視窗寬度大於等於 px時執行
         $('.navbar .container div').removeClass('dropdown-menu');
         $('.navbar .container div ul').removeClass('dropdown-item');
-        // container
-        $('.index1 .row:nth-child(1) .col').addClass('col-6');
-        $('.index1 .row:nth-child(1) .col').removeClass('col');
-        $('.index1 .row:nth-child(2) .col').addClass('col-8');
-        $('.index1 .row:nth-child(2) .col').removeClass('col');
-        $('.index1 .row:nth-child(3)>div:nth-child(1)').addClass('col-2');
-        $('.index1 .row:nth-child(3)>div:nth-child(2)').addClass('col-9');
-        $('.index2').innerHTML = `                <div class="row">
-        <div class="col-2">
-            <a href="./Neiw.html"><img src="./img/index/index-btn-Neiw.png" alt="前進內灣線"></a>
-        </div>
-        <div class="col-2">
-            <a href="./Liuj.html"><img src="./img/index/index-btn-Liuj.png" alt="前進六家線"></a>
-        </div>
-        <div class="col-2">
-            <a href="./Jiji.html"><img src="./img/index/index-btn-Jiji.png" alt="前進集集線"></a>
-        </div>
-        <div class="col-2">
-            <a href="./Shal.html"><img src="./img/index/index-btn-Shal.png" alt="前進沙崙線"></a>
-        </div>
-        <div class="col-2">
-            <a href="./Shen.html"><img src="./img/index/index-btn-Shen.png" alt="前進深澳線"></a>
-        </div>
-        <div class="col-2">
-            <a href="./Ping.html"><img src="./img/index/index-btn-Ping.png" alt="前進平溪線"></a></div>
-    </div>`;
+
+        // 網頁版2欄位btn
+        document.querySelector('.index2').innerHTML = `
+        <div class="row">
+            <div class="col-2">
+                <a href="./Neiw.html"><img src="./img/index/index-btn-Neiw.png" alt="前進內灣線"></a>
+            </div>
+            <div class="col-2">
+                <a href="./Liuj.html"><img src="./img/index/index-btn-Liuj.png" alt="前進六家線"></a>
+            </div>
+            <div class="col-2">
+                <a href="./Jiji.html"><img src="./img/index/index-btn-Jiji.png" alt="前進集集線"></a>
+            </div>
+            <div class="col-2">
+                <a href="./Shal.html"><img src="./img/index/index-btn-Shal.png" alt="前進沙崙線"></a>
+            </div>
+            <div class="col-2">
+                <a href="./Shen.html"><img src="./img/index/index-btn-Shen.png" alt="前進深澳線"></a>
+            </div>
+            <div class="col-2">
+                <a href="./Ping.html"><img src="./img/index/index-btn-Ping.png" alt="前進平溪線"></a></div>
+        </div>`;
     }
 };
+
+function mq2() {
+    var query2 = Modernizr.mq('(max-width:767.98px)');
+    if (query2) {
+        $('#holder').html('true');
+        // JavaScript here
+        // 當CSS media query計算的視窗寬度小於769px時執行
+        document.querySelector('.index2').innerHTML = `
+        <div class="row">
+            <div class="col-sm-12">
+                <a href="./Neiw.html"><img src="./img/index/index-btn-Neiw-mobi.jpg" alt="前進內灣線"></a>
+            </div>
+            <div class="col-sm-12">
+                <a href="./Liuj.html"><img src="./img/index/index-btn-Liuj-mobi.jpg" alt="前進六家線"></a>
+            </div>
+            <div class="col-sm-12">
+                <a href="./Jiji.html"><img src="./img/index/index-btn-Jiji-mobi.jpg" alt="前進集集線"></a>
+            </div>
+            <div class="col-sm-12">
+                <a href="./Shal.html"><img src="./img/index/index-btn-Shal-mobi.jpg" alt="前進沙崙線"></a>
+            </div>
+            <div class="col-sm-12">
+                <a href="./Shen.html"><img src="./img/index/index-btn-Shen-mobi.jpg" alt="前進深澳線"></a>
+            </div>
+            <div class="col-sm-12">
+                <a href="./Ping.html"><img src="./img/index/index-btn-Ping-mobi.jpg" alt="前進平溪線"></a>
+            </div>
+        </div>`;
+        console.log(document.querySelector('.index2').innerHTML, $(window).width());
+    } else {
+        $('#holder').html('false');
+        // JavaScript here
+        // 當CSS media query計算的視窗寬度大於等於 px時執行
+    }
+};
+
+
 $(window).resize(function () {
-    windowSize();
+    // windowSize();
+    mq();
+    mq2();
 });
-windowSize();
+// windowSize();
+mq();
+mq2();
